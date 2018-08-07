e3f09cde699ea53e3ad549c2a7a0503725c18d5d
i.chavez.romero <i.chavez.romero@accenture.com>
2018-08-06 23:46:28 -0500

    Mi primer commit

---
+++ Push_led.ino
@@ -0,0 +1,13 @@
+byte push=3, led=4;
+void setup() {
+pinMode(push, INPUT);
+pinMode(led, OUTPUT);
+}
+
+void loop() {
+if(push==HIGH){
+  digitalWrite(led, HIGH);
+  delay(1500);
+  digitalWrite(led, LOW);
+  }
+}
---
+++ eg2.py
@@ -0,0 +1,2 @@
+print('h')
+def cffi im
